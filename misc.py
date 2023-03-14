53583928e27d0390ee6a219bbd761319bd8af500
Chris <chrisandrew119@gmail.com>
2023-03-15 04:57:02 +0530

    Choose

--- misc.py
+++ misc.py
@@ -1,3 +1,4 @@
+import random
 
 def fun_commands(message):
     """Fun commands that can be added here"""
@@ -17,3 +18,16 @@ def fun_commands(message):
     # if "contest" in message.content.lower():
     #     response = 'Grats Matt'
     return response
+
+
+def choose_handler(message):
+    """Handler code"""
+    values = message.content.split()
+    USAGE = "!choose a b c"
+    if len(values) == 1 and values[0] == "!choose":
+        return USAGE
+
+    if len(values) >= 2 and values[0] == "!choose":
+        choices = values[1::]
+        choice = random.choice(choices)
+        return choice
