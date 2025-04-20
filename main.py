def2c9c3a66f53897e23c2777b6994cf22db23d6
Chris <chrisandrew119@gmail.com>
2025-04-20 18:36:53 +0000

    Removing flask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import discord
 import os
-from keep_alive import keep_alive
 from misc import fun_commands, choose_handler
 from trainer_id import trainer_id_handler
 from rarity import rarity_handler
